572dd3f2c748c993ce4088ba0de3c2ff2fb27798
jmattgiroux <jmattgiroux@gmail.com>
2022-07-04 11:52:30 -0600

    working on getting the test graphql app to work with Heroku

---
+++ src/backend/database/database-routes.js
@@ -0,0 +1,8 @@
+// entry route for the app
+
+const router = require("express").Router();
+
+// router.use("/user", require("./user-features/user-routes"));
+// router.use("/api-docs", require("./user-features/user-routes"));
+
+module.exports = router;
