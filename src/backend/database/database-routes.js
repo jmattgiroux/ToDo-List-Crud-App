3fa00712d42c870af9d9b87416b2f57a65c017b4
jmattgiroux <jmattgiroux@gmail.com>
2022-07-04 10:29:10 -0600

    Restarted project in order to get graphql working; now partway through getting graphql set up for the project

--- src/backend/database/database-routes.js
+++
@@ -1,8 +0,0 @@
-// entry route for the app
-
-const router = require("express").Router();
-
-router.use("/user", require("./user-features/user-routes"));
-router.use("/api-docs", require("./user-features/user-routes"));
-
-module.exports = router;
